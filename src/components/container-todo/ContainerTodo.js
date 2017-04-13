71e35bd47bb13ed1637fe05c0c228f7282496afb
havietduc91 <havietduc91@gmail.com>
2017-04-13 15:27:18 +0700

    Add list of material components

--- src/components/container-todo/ContainerTodo.js
+++ src/components/container-todo/ContainerTodo.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, {Component, PropTypes}from 'react';
 import { connect } from 'react-redux';
 
 import Todos from '../todos/Todos';
 import AddTodo from '../add-todo/AddTodo';
 import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
+import {List, ListItem, makeSelectable} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import Subheader from 'material-ui/Subheader';
+
+let SelectableList = makeSelectable(List);
+
+function wrapState(ComposedComponent) {
+    return class SelectableList extends Component {
+        static propTypes = {
+            children: PropTypes.node.isRequired,
+            defaultValue: PropTypes.number.isRequired,
+        };
+
+        componentWillMount() {
+            this.setState({
+                selectedIndex: this.props.defaultValue,
+            });
+        }
+
+        handleRequestChange = (event, index) => {
+            this.setState({
+                selectedIndex: index,
+            });
+        };
+
+        render() {
+            return (
+                <ComposedComponent
+                    value={this.state.selectedIndex}
+                    onChange={this.handleRequestChange}
+                >
+                    {this.props.children}
+                </ComposedComponent>
+            );
+        }
+    };
+}
+
+SelectableList = wrapState(SelectableList);
 
 class ContainerTodo extends React.Component {
     constructor(props) {
@@ -18,7 +57,7 @@ class ContainerTodo extends React.Component {
 
     render() {
         let {todos} = this.props;
-        
+
         return (
             <div id="container-todo">
                 <AddTodo/>
@@ -28,10 +67,41 @@ class ContainerTodo extends React.Component {
                     autoOk={true}
                     floatingLabelText="Submission Start:"
                 />
+                <SelectableList defaultValue={3}>
+                    <Subheader>Selectable Contacts</Subheader>
+                    <ListItem
+                        value={1}
+                        primaryText="Brendan Lim"
+                        leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                        nestedItems={[
+                          <ListItem
+                            value={2}
+                            primaryText="Grace Ng"
+                            leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
+                          />,
+                        ]}
+                    />
+                    <ListItem
+                        value={3}
+                        primaryText="Kerem Suer"
+                        leftAvatar={<Avatar src="images/kerem-128.jpg" />}
+                    />
+                    <ListItem
+                        value={4}
+                        primaryText="Eric Hoffman"
+                        leftAvatar={<Avatar src="images/kolage-128.jpg" />}
+                    />
+                    <ListItem
+                        value={5}
+                        primaryText="Raquel Parrado"
+                        leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
+                    />
+                </SelectableList>
             </div>
         );
     }
 }
+
 
 const mapStateToProp = (store) => {
     return {
